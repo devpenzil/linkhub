ed958e0b2612ec3cfdc4c3e72afd9c023b5dd1e3
devpenzil <ajoalex012@gmail.com>
2022-05-07 21:36:15 +0530

    change endpoints

--- appwrite/config.js
+++ appwrite/config.js
@@ -2,7 +2,7 @@ import { Appwrite } from "appwrite";
 const app = new Appwrite();
 
 app
-  .setEndpoint("http://ec2-65-1-110-75.ap-south-1.compute.amazonaws.com/v1")
+  .setEndpoint("https://ec2-65-1-110-75.ap-south-1.compute.amazonaws.com/v1")
   .setProject("6276945952266f6d7951");
 
 export default app;
